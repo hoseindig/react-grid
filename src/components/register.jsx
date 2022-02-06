bbbd597bdddb93e4bf89f5d10bb5632ba77ea456
hoseindig <hosein_dig@yahoo.com>
2022-02-06 15:50:22 +0330

    add service call to form

--- src/components/register.jsx
+++ src/components/register.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./form";
+import { toast } from "react-toastify";
+
+
+import * as userService from '../services/userService'
+
 class Register extends Form {
   state = {
     data: {
-      username: "",
-      password: "",
+      username: "aa@aa.cc",
+      name: "aa",
+      password: "11111",
     },
     errors: {},
   };
@@ -15,12 +21,28 @@ class Register extends Form {
     password: Joi.string().min(5).required().label("کلمه عبور"),
     name: Joi.string().required().label("نام"),
   };
+
+  doSubmit = async () => {
+    try {
+      await userService.register(this.state.data)
+    } catch (ex) {
+      debugger
+      if (ex.response && ex.response.status === 400) {
+        toast.error(this.state.errors)
+
+        const errors = { ...this.state.errors }
+        errors.username = ex.response.data
+        this.setState({ errors })
+
+      }
+    }
+  }
   render() {
     return (
       <div>
         <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("username", "UserName")}
+          {this.renderInput("username", "Username")}
           {this.renderInput("password", "Password", "password")}
           {this.renderInput("name", "Name")}
 
